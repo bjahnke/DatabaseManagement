e37f6abf6296c500229963f1d79b9481dc3323b7
bjahnke <bjahnke71@gmail.com>
2016-02-11 20:01:43 -0500

    labeled each query with numbers

--- lab3.sql
+++ lab3.sql
@@ -1,4 +1,6 @@
-﻿----------------------------------------------------------------------------------------
+﻿--Brian Jahnke
+
+----------------------------------------------------------------------------------------
 -- Postgres create, load, and query script for CAP2.
 --
 -- SQL statements for the CAP2 database
@@ -182,42 +184,48 @@ INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
   VALUES(1026, 'may', 'c002', 'a05', 'p03',  800, 740.00);
 
 
--- SQL statements for displaying example data into the CAP2 database
--- Connect to your Postgres server and set the active database to CAP2.  Then . . .
 
-
+--1.)
 select ordno, dollars
 From Orders;
 
+--2.)
 select name, city
 from agents WHERE name = 'Smith';
 
+--3.)
 select pid, name, priceUSD 
 From products
 where quantity > 208000;
 
+--4.)
 select name, city
 from customers
 Where city = 'Dallas';
 
+--5.)
 select name
 from agents
 where city NOT IN('New York', 'Tokyo');
 
+--6.)
 select *
 from products
 where city NOT IN('Dallas', 'Duluth')
 and priceUSD >= 1;
 
+--7.)
 select * 
 from orders
 where mon IN('jan', 'mar');
 
+--8.)
 select *
 from orders
 where mon = 'feb'
 and dollars < 500;
 
+--9.)
 select *
 from orders 
 where cid = 'C005';
