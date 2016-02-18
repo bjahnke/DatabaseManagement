a5c487dab55dad3325a78c995c183d242e0eae58
bjahnke <bjahnke71@gmail.com>
2016-02-18 01:41:11 -0500

    Lab4 completed

--- Lab4.sql
+++ Lab4.sql
@@ -263,5 +263,22 @@ where cid in( select cid
 
 --8.)
 
+-- A check constraint is a requirement that may be set to one or multiple columns.
+-- A value must meet the criteria of a given check constrain of a column before
+-- it can be set as a value to that column. Check constrains are usefull for
+-- preventing unintended cases from occuring in the database that would otherwise 
+-- reduse its usefullnes or even break it. An example of a good check constraint 
+-- would be a value can not equal one that already exists in the column if that 
+-- column is meant to have unique values. This is a good constraint because if 
+-- someone wants to search a table for information about a specific customer,
+-- the constraint will prevent confusing cases such as 2 different customers 
+-- with different data having the same name. A bad constraint would be making 
+-- it so the city a customer is located in must be unique. This is a bad 
+-- constraint because, unlike the first case, it is reasonable to have multiple
+-- customers being located in the same city and preventing this would only
+-- reduce the amount of data a database should be able to hold.   
+
+
+
 
 
